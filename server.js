1430f8ed413131e06de106fb9e748479517291ce
erica2605@live.com <erica2605@live.com>
2019-05-28 14:33:29 -0700

    added mongoose

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => res.render('index', {}))
+
+// CREATE POST
+
+app.get('/posts/new', (req, res) => res.render('posts-new'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
