cdb0bdb7c7d3f109ca87f20af507fc43ec25e777
erica2605@live.com <erica2605@live.com>
2019-05-28 11:39:10 -0700

    i'm trying my best

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const exphbs = require('express-handlebars')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => res.render('index', {}))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
