8244b25764c85fdc430141eeece7ee989adb3047
erica2605@live.com <erica2605@live.com>
2019-05-30 12:36:53 -0700

    working on posts

--- server.js
+++ server.js
@@ -5,13 +5,39 @@ const exphbs = require('express-handlebars')
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+mongoose.connect('mongodb://localhost/reddit', { useNewUrlParser: true });
+
+app.use(bodyParser.urlencoded({ extended: true }))
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', 'hbs');
 
-app.get('/', (req, res) => res.render('index', {}))
+const Post = mongoose.model('Post', {
+  title: String,
+  body: String
+})
+
+app.get('/', (req, res) => {
+  Post.find()
+    .then(posts => {
+      res.render('posts-index', {posts: posts})
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
 
 // CREATE POST
 
-app.get('/posts/new', (req, res) => res.render('posts-new'))
+app.get('/posts/new', (req, res) => res.render('posts-new', {}))
+
+// CREATE
+app.post('/posts', (req, res) => {
+  Post.create(req.body).then((post) => {
+    console.log(post)
+    res.redirect('/')
+  }).catch((err) => {
+    console.log(err.message)
+  })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
