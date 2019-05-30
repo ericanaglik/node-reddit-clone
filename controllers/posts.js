5a2924c26a4da1f263c7f88a9a65d500be1d45dd
erica2605@live.com <erica2605@live.com>
2019-05-30 13:29:28 -0700

    Users can view a list of posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,17 +1,32 @@
 const Post = require('../models/post');
 
-module.exports = (app) => {
-    
-      // CREATE
-      app.post('/posts/new', (req, res) => {
-        // INSTANTIATE INSTANCE OF POST MODEL
-        const post = new Post(req.body);
-    
-        // SAVE INSTANCE OF POST MODEL TO DB
-        post.save((err, post) => {
-          // REDIRECT TO THE ROOT
-          return res.redirect(`/`);
-        })
-      });
-    
-    };
+module.exports = (app) => {  
+  
+  // CREATE
+  app.post('/posts/new', (req, res) => {
+    // INSTANTIATE INSTANCE OF POST MODEL
+    const post = new Post(req.body);
+    // SAVE INSTANCE OF POST MODEL TO DB
+    post.save((err, post) => {
+      // REDIRECT TO THE ROOT
+      return res.redirect(`/`);
+    })
+  });
+
+  // CREATE FORM
+  app.get('/posts/new', (req, res) => {
+    res.render('posts-new', {})
+  })
+
+  // INDEX
+  app.get('/', (req, res) => {
+    Post.find()
+      .then(posts => {
+        res.render('posts-index', {posts: posts})
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  })
+
+};
--- server.js
+++ server.js
@@ -6,9 +6,6 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 
-// Set db
-require('./data/reddit-db');
-
 mongoose.connect('mongodb://localhost/reddit', { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -16,33 +13,11 @@ app.use(expressValidator())
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', 'hbs');
 
-const Post = mongoose.model('Post', {
-  title: String,
-  body: String
-})
+require('./controllers/posts.js')(app);
+require('./data/reddit-db');
+// require('./controllers/comments.js')(app);
+// require('./controllers/auth.js')(app);
+// require('./controllers/replies.js')(app);
 
-app.get('/', (req, res) => {
-  Post.find()
-    .then(posts => {
-      res.render('posts-index', {posts: posts})
-    })
-    .catch(err => {
-      console.log(err)
-    })
-})
-
-// CREATE POST
-
-app.get('/posts/new', (req, res) => res.render('posts-new', {}))
-
-// CREATE
-app.post('/posts', (req, res) => {
-  Post.create(req.body).then((post) => {
-    console.log(post)
-    res.redirect('/')
-  }).catch((err) => {
-    console.log(err.message)
-  })
-})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
