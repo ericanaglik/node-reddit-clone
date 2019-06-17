be473426cdcf11e3e91fc0ae884d8069fabb330a
erica2605@live.com <erica2605@live.com>
2019-06-16 22:54:12 -0700

    Authors for posts are visible

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,18 +3,19 @@ const Comment = require('../models/comment');
 
 module.exports = app => {
 
-    //INDEX (SHOW ALL POSTS)
-    app.get("/", (req, res) => {
+    // INDEX
+    app.get('/', (req, res) => {
         var currentUser = req.user;
-      
-        Post.find({})
-          .then(posts => {
-            res.render("posts-index", { posts, currentUser });
-          })
-          .catch(err => {
+        // res.render('home', {});
+        console.log(req.cookies);
+        Post.find().populate('author')
+        .then(posts => {
+            res.render('posts-index', { posts, currentUser });
+            // res.render('home', {});
+        }).catch(err => {
             console.log(err.message);
-          });
-    });
+        })
+    })
 
     //NEW POST FORM
     app.get('/posts/new', (req, res) => {
@@ -24,27 +25,38 @@ module.exports = app => {
     // CREATE
     app.post("/posts/new", (req, res) => {
         if (req.user) {
-        var post = new Post(req.body);
-    
-        post.save(function(err, post) {
-            return res.redirect(`/`);
-        });
+            var post = new Post(req.body);
+            post.author = req.user._id;
+            post
+                .save()
+                .then(post => {
+                    return User.findById(req.user._id);
+                })
+                .then(user => {
+                    user.posts.unshift(post);
+                    user.save();
+                    // REDIRECT TO THE NEW POST
+                    res.redirect(`/posts/${post._id}`);
+                })
+                .catch(err => {
+                    console.log(err.message);
+                });
         } else {
         return res.status(401); // UNAUTHORIZED
         }
     });
 
 
-    //SHOW (INDIVISUAL POST)
+    //SHOW (INDIVIDUAL POST)
     app.get("/posts/:id", function(req, res) {
         // // LOOK UP THE POST
-        // Post.findById(req.params.id)
-        //   .then(post => {
-        //     res.render("posts-show", { post });
-        //   })
-        //   .catch(err => {
-        //     console.log(err.message);
-        //   });
+        Post.findById(req.params.id)
+          .then(post => {
+            res.render("posts-show", { post });
+          })
+          .catch(err => {
+            console.log(err.message);
+          });
         // LOOK UP THE POST
         Post.findById(req.params.id).populate('comments').then((post) => {
             res.render('posts-show', { post })
@@ -54,14 +66,15 @@ module.exports = app => {
     });
 
     // SUBREDDIT
-    app.get("/n/:subreddit", function(req, res) {
-        Post.find({ subreddit: req.params.subreddit })
-        .then(posts => {
-            res.render("posts-index", { posts });
-        })
-        .catch(err => {
-            console.log(err);
-        });
+    app.get("/n/:subreddit", function (req, res) {
+        var currentUser = req.user;
+        Post.find({ subreddit: req.params.subreddit }).populate('author')
+            .then(posts => {
+                res.render("posts-index", { posts, currentUser });
+            })
+            .catch(err => {
+                console.log(err);
+            });
     });
 
 };
